0270d694ffa84967098f7037de9b930944b60760
hrt-takeuchi <157057th@edu.setsunan.ac.jp>
2018-02-12 11:44:43 +0900

    test

---
+++ app/channels/chat_message_channel.rb
@@ -0,0 +1,13 @@
+class ChatMessageChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat_message_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    ChatMessage.create! user_id: current_user.id, body: data['message']
+  end
+end
